8d240399190a87c4e20a4577cb34f267fa16ce7f
Jinren Ping <jinren.ping@outlook.com>
2018-12-07 17:26:46 -0500

    update upload

--- src/api.js
+++ src/api.js
@@ -2,7 +2,7 @@ import apigClientFactory from "aws-api-gateway-client";
 import { fileToBase64 } from "./util";
 
 var awsConfig = {
-  invokeUrl: "https://4sfad70fh6.execute-api.us-east-1.amazonaws.com/dev",
+  invokeUrl: "https://4sfad70fh6.execute-api.us-east-1.amazonaws.com/qa",
   apiKey: "57aJGBCTbx5yQOlR5CuKkqSkcAGP4ROwdlHhphc0",
   region: "us-east-1"
 };
@@ -26,18 +26,25 @@ export const uploadImage = async file => {
   const pathTemplate = `/photos/${file.name}`;
   const additionalParams = {
     headers: {
-      "Content-Type": file.type
+      "Content-Type": "text/plain"
     }
   };
-  // const body = await fileToBase64(file)
-  const body = { file };
-  console.log(file);
+  const encodedFile = await fileToBase64(file);
+  var encodedImage;
+  if (file.type === "image/jpeg") {
+    encodedImage = encodedFile.substring(23);
+  } else {
+    encodedImage = encodedFile.substring(22);
+  }
+  console.log(file.type);
+  console.log(encodedFile);
+  console.log(encodedImage);
   return apiClient.invokeApi(
     null,
     pathTemplate,
     method,
     additionalParams,
-    body
+    encodedImage
   );
 };
 
